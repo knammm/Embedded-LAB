c0f66a39daf0d805c5a65756a532c05242261953
olkmphy <nam.nguyenolkmphy@hcmut.edu.vn>
2024-09-19 14:40:02 +0700

    Update Lab 1 ex1

--- BKIT_ARM4_Bai2_Timer_7seg-main/Bai2_Timer_7seg/Core/Src/main.c
+++ BKIT_ARM4_Bai2_Timer_7seg-main/Bai2_Timer_7seg/Core/Src/main.c
@@ -103,13 +103,10 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  while(!flag_timer2);
-	  flag_timer2 = 0;
-	  // main task, every 50ms
-	  test_LedDebug();
-	  test_LedY0();
-	  test_LedY1();
-	  test_7seg();
+	  HAL_GPIO_WritePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin, 1);
+	  HAL_Delay(2000);
+	  HAL_GPIO_WritePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin, 0);
+	  HAL_Delay(4000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -168,7 +165,7 @@ void system_init(){
 	  HAL_GPIO_WritePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin, 0);
 	  timer_init();
 	  led7_init();
-	  setTimer2(50);
+	  //setTimer2(50);
 }
 
 uint8_t count_led_debug = 0;
